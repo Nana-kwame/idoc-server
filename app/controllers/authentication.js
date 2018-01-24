6893f3d837e60f95f368e1c8944155c55ed90d79
Nana Kwame Adomako Acheampong <nacheampong123@gmail.com>
2018-01-24 00:40:21 +0000

    first commit

---
+++ app/controllers/authentication.js
@@ -0,0 +1,90 @@
+
+var jwt = require('jsonwebtoken');
+var User = require('../models/user');
+var authConfig = require('../../config/auth');
+
+function generateToken(user) {
+    return jwt.sign(user, authConfig.secret,{
+        expiresIn:10080
+    });
+}
+
+function setUserInfo(request) {
+    
+    return{
+        _id: request._id,
+        fName:request.fName,
+        lName: request.lName,
+        email: request.email,
+        sex: request.sex,
+        DOB: request.DOB,
+        nationality: request.nationality,
+        hosID: request.hosID
+    };
+}
+
+exports.login = function(req,res,next){
+    
+        var userInfo = setUserInfo(req.user);
+    
+        res.status(200).json({
+            token: 'JWT' + generateToken(userInfo),
+            user: userInfo
+        });
+    }
+
+    exports.register = function(req, res,next){
+        
+            var fName = req.body.fName;
+            var lName = req.body.lName;
+            var email = req.body.email;
+            var password= req.body.password;
+            var sex = req.body.sex;
+            var DOB = req.body.DOB;
+            var nationality = req.body.nationality;
+            
+           
+        
+            if(!email){
+                return res.status(422).send({error: 'You must enter a valid email address'});
+            }
+        
+            if(!password){
+                return res.status(422).send({error: 'You must enrer a password'});
+            }
+        
+            User.findOne({email: email}, function(err, existingUser){
+        
+                if(err){
+                    return next(err);
+                }
+        
+                if(existingUser){
+                    return res.status(422).send({error:'Email address is already in use'});
+                }
+        
+                var user = new User({
+                    email: email,
+                    password: password,
+                    fName: fName,
+                    lName: lName,
+                    sex: sex,
+                    DOB: DOB,
+                    nationality: nationality
+                });
+        
+                user.save(function(err,user){
+        
+                    if(err){
+                      return next(err);  
+                    }
+        
+                    var userInfo = setUserInfo(user);
+        
+                    res.status(201).json({
+                        token: 'JWT' + generateToken(userInfo),
+                        user: userInfo
+                    })
+                })  
+            })
+        }
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //'url': 'mongodb://<nanaAdmin>:<king>@ds123456.mlab.com:49466/mongodbroles'
+    'url': 'mongodb://127.0.0.1/iDoc'
+    
+}
