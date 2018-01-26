bacbb2ca6be2db1e4af4d8f1c7a6f75df7179e2c
Nana Kwame Adomako Acheampong <nacheampong123@gmail.com>
2018-01-26 20:43:55 +0000

    Changed Database config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //'url': 'mongodb://<nanaAdmin>:<king>@ds123456.mlab.com:49466/mongodbroles'
-    'url': 'mongodb://127.0.0.1/iDoc'
+    'url': 'mongodb://nanaKwame:king123@ds123456.mlab.com:49466/iDoc'
+    //'url': 'mongodb://127.0.0.1/iDoc'
     
 }
