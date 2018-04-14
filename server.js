3becac461aff8f06ce4986c9d7f5bf965702cad2
Nana Kwame Adomako Acheampong <nacheampong123@gmail.com>
2018-04-14 10:14:34 +0000

    Modified server.js

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ app.use(cors());
 router(app);
 
 /*WATSON DISCOVERY*/
-app.use( function (req,res,next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+// app.use( function (req,res,next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header('Access-Control-Allow-Methods', 'DELETE, PUT, POST');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
 
 app.post('/watsonHospitals', function (req, res) {
     
