6a61f5107dc1f21ab23b5ba2c1cdd884e6678002
DO-ENR-LAP-5\Nana Kwame Acheampon <nana.acheampong@dreamoval.com>
2018-11-28 08:14:44 +0000

    Added the port to the mlab url

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'url': 'mongodb://nanaKwame:king123@ds129024.mlab.com:17178/idoc'
+    'url': 'mongodb://nanaKwame:king123@ds129024.mlab.com:29024/idoc'
     // 'url': 'mongodb://127.0.0.1/iDoc'
     
 }
